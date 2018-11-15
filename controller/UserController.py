5df13279cb09ba8c92d136e02e38c0d394f4d6d5
Nick Erokhin <nickerokhin@gmail.com>
2018-11-14 20:48:35 -0700

    parses search query

--- controller/UserController.py
+++ controller/UserController.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('../')
+sys.path.append('../models')
+sys.path.append('../views')
 import Command
 import DocumentController
 
@@ -13,8 +17,48 @@ class UserController:
         self.arguments = arguments
 
     def parseArguments(self):
-        pass
+        args = self.arguments.split(" ")
+        for i in range(0, len(args)):
+            if args[i] == "search":
+                if args[i + 1] == "q/":
+                    #find closing tag
+                    if "/q" not in args:
+                        print('Invalid query, do you have the proper "q/" and "/q" tags in place?\n')
+                        break
 
-docCont = DocumentController.DocumentController(None)
+                    f = i + 2
+
+                    while f < len(args):
+                        if args[f] == "/q":
+                            break
+                        else:
+                            f += 1
+
+                    querytokens = args[i + 2: f]
+                    querytokens = " ".join(querytokens)
+                    querytokens = querytokens.split(",")
+                    for i in range(0, len(querytokens)):
+                        querytokens[i] = querytokens[i].strip()
+                    print(querytokens)
+                        
+
+
+    def run(self):
+        #Check for existing searchIndex here 
+
+        print('''Welcome to ImageSearch. \nYou have {} images in the index\nFor instructions, type "man"\nPlease type your instructions below...
+                 
+                 
+                  '''.format(5))
+        while True:
+            args = input(">")
+            self.setArguments(args)
+            self.parseArguments()
+
+
+
+
+userCont = UserController()
+userCont.run()
 
 
