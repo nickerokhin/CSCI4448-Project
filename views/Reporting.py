6808ae15fb86264276c451857f2ecb5f4d11c964
Nick Erokhin <nickerokhin@gmail.com>
2018-11-29 05:36:51 -0700

    Making sure everything up to date

--- views/Reporting.py
+++ views/Reporting.py
@@ -8,6 +8,9 @@ class Reporting:
         for im in ims:
             print("Name: {}\n Tags: {}\n".format(im.getName(), im.getTags()))
 
+    def printTags(self, tags):
+        print(', '.join(tags))
+
     def viewImages(self, ims):
 
         for im in ims:
@@ -24,6 +27,30 @@ class Reporting:
         print("Listing documents:")
         for r in res:
             print(r.getName())
+
+    def printMan(self):
+        print('''
+To search for an image using tags, format your query like so:
+
+- search q/ term1, term2, term3 /q
+
+To add an image to the index, format your query like so:
+
+- add {num to add} pic1.jpg pic2.jpg
+
+To save the search index to disk, type 'save'
+
+To view the images from the search results, format your query like so:
+
+- showResultIms {num of images to show}
+
+To list all documents in the index, type 'ls'
+
+You able to stack commands as well.
+If you want to add images and save the index with one command, the following will work
+
+- add 2 pic1.jpg pic2.jpg save
+        ''')
 
 
 
