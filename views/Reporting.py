dfb7a1c6404a8ffce1aa6a0b9ba78f2fcbdd5ee4
Nick Erokhin <nickerokhin@gmail.com>
2018-11-28 18:30:11 -0700

    Added a view to display images using opencv

--- views/Reporting.py
+++ views/Reporting.py
@@ -1,9 +1,23 @@
+import cv2
+import numpy as np
+
 class Reporting:
 
     def viewAddResults(self, ims):
         print("Successfully added {} image(s)".format(len(ims)))
         for im in ims:
             print("Name: {}\n Tags: {}\n".format(im.getName(), im.getTags()))
+
+    def viewImages(self, ims):
+
+        for im in ims:
+            docPath = im.getDocumentPath()
+            img = cv2.imread(docPath, 0)
+            imName = im.getName()
+            cv2.imshow(imName, img)
+            
+
+
 
     
 
